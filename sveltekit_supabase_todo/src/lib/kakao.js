5acecf99c0c4404c745f981b247c272d71dbf445
조수정 <josujeong00@mintech.kr>
2023-02-16 02:00:32 +0900

    build: 환경변수 정리

--- sveltekit_supabase_todo/src/lib/kakao.js
+++ sveltekit_supabase_todo/src/lib/kakao.js
@@ -1,11 +1,11 @@
 import {
+  PUBLIC_BASE_URL,
   PUBLIC_KAKAO_REST_API_KEY,
-  PUBLIC_KAKAO_REDIRECT_URI
 } from '$env/static/public';
 
 const kakaoAuthUri = new URL("https://kauth.kakao.com");
 kakaoAuthUri.searchParams.append("client_id", PUBLIC_KAKAO_REST_API_KEY);
-kakaoAuthUri.searchParams.append("redirect_uri", PUBLIC_KAKAO_REDIRECT_URI);
+kakaoAuthUri.searchParams.append("redirect_uri", `${PUBLIC_BASE_URL}/sign_in/kakao/callback`);
 kakaoAuthUri.searchParams.append("response_type", "code");
 kakaoAuthUri.searchParams.append("scope", "account_email");
 
