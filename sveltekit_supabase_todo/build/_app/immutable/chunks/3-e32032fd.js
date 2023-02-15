5ed56f2dc22d79c95151d11cbef31b7459f2749d
조수정 <josujeong00@mintech.kr>
2023-02-15 17:09:18 +0900

    build: build 폴더 삭제

--- sveltekit_supabase_todo/build/_app/immutable/chunks/3-e32032fd.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/posts/hello/_page.md-90bb0ff7.js";export{t as component};
--- sveltekit_supabase_todo/build/_app/immutable/chunks/4-4c086272.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_page-a53fb96a.js";import{default as t}from"../components/pages/todos/_page.md-42ffe3d1.js";export{t as component,r as universal};
