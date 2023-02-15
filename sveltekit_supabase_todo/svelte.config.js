0e4299c0912096e85a66380baa8a56eb27013027
조수정 <josujeong00@mintech.kr>
2023-02-15 17:26:08 +0900

    build: @sveltejs/adapter-netlify 적용

--- sveltekit_supabase_todo/svelte.config.js
+++ sveltekit_supabase_todo/svelte.config.js
@@ -1,5 +1,6 @@
 // import adapter from '@sveltejs/adapter-static'
-import adapter from '@sveltejs/adapter-auto'
+// import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-netlify'
 import {
   mdsvex
 } from 'mdsvex'
@@ -13,7 +14,16 @@ const config = {
   preprocess: [preprocess(), mdsvex(mdsvexConfig)],
 
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      // if true, will create a Netlify Edge Function rather
+      // than using standard Node-based functions
+      edge: false,
+ 
+      // if true, will split your app into multiple functions
+      // instead of creating a single one for the entire app.
+      // if `edge` is true, this option cannot be used
+      split: false
+    }),
   },
 }
 
